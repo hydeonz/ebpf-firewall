ae170959314070e3974d5622906daab64eb7d1a6
Юрий <yurakirin9545@gmail.com>
2025-04-22 15:42:47 +0300

    DEBUG

--- bpf/analyze.c
+++ bpf/analyze.c
@@ -6,6 +6,11 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
+// Добавляем макрос для отладки
+#define bpf_debug(fmt, ...) \
+    ({ char ____fmt[] = fmt; \
+       bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); })
+
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(key_size, sizeof(__u32));
@@ -23,6 +28,9 @@ int analyze_connections(struct xdp_md *ctx)
     if ((void*)(eth + 1) > data_end)
         return XDP_PASS;
 
+    // Добавляем отладочный вывод для Ethernet
+    bpf_debug("Got packet, eth protocol: %x\n", bpf_ntohs(eth->h_proto));
+
     if (eth->h_proto != bpf_htons(ETH_P_IP))
         return XDP_PASS;
 
@@ -30,12 +38,19 @@ int analyze_connections(struct xdp_md *ctx)
     if ((void*)(ip + 1) > data_end)
         return XDP_PASS;
 
+    // Добавляем отладочный вывод для IP
+    bpf_debug("IP protocol: %d, src: %x\n", ip->protocol, ip->saddr);
+
     if (ip->protocol != IPPROTO_TCP)
         return XDP_PASS;
 
     struct tcphdr *tcp = (void*)(ip + 1);
     if ((void*)(tcp + 1) > data_end)
         return XDP_PASS;
+
+    // Добавляем отладочный вывод для TCP
+    bpf_debug("TCP src_port: %d, dst_port: %d\n", 
+              bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest));
 
     __u32 src_ip = ip->saddr;
     __u32 *count = bpf_map_lookup_elem(&connection_map, &src_ip);
@@ -46,6 +61,9 @@ int analyze_connections(struct xdp_md *ctx)
     }
 
     bpf_map_update_elem(&connection_map, &src_ip, &new_count, BPF_ANY);
+    
+    // Добавляем отладочный вывод для счетчика
+    bpf_debug("Updated connection count: %d\n", new_count);
 
     return XDP_PASS;
 }
